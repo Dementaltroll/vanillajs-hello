e73f6a9a1d0d7ab4d9aa267b226d6fbc58f2b338
Dementaltroll <134567062+Dementaltroll@users.noreply.github.com>
2024-10-26 20:58:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarExcusa() {
+      let who = ['Mi tía', 'El Paul', 'La Tamara', 'El Jhonny'];
+      let action = ['meo', 'destruyó', 'masticó', 'desapareció'];
+      let what = ['mi celular', 'a mi mejor amiga', 'a tu hermana'];
+      let when = ['en clases de pilates', 'en vivo por cadena nacional', 'cuando fumaba', 'mientras lloraba', 'el día que todo se fue a la shit'];
+
+      let randomWho = who[Math.floor(Math.random() * who.length)];
+      let randomAction = action[Math.floor(Math.random() * action.length)];
+      let randomWhat = what[Math.floor(Math.random() * what.length)];
+      let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+      return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  document.getElementById("excuse").innerHTML = generarExcusa();
 };
